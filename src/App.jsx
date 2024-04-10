2d5f4ebab210e51f7fdcd89d65a439cc5ac77773
tomg195 <gardner195@msn.com>
2024-04-10 18:33:41 +0100

    image sourcing test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import BookingCalendar from "./BookingCalendar";
 
 function App() {
   const images = [
-    { url: "https://andyhardingholidayhome.netlify.app/images/IMG1.jpg" },
+    { url: "http://localhost:5173//IMG1.jpg" },
     { url: "/IMG2.jpg" },
     { url: "/IMG3.jpg" },
     { url: "/IMG4.jpg" },
