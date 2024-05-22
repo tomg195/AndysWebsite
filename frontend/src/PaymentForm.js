abd62ccdf10e37b3ce2717ceea381c8036167eb3
tomg195 <gardner195@msn.com>
2024-05-22 21:57:48 +0100

    modified files

--- frontend/src/PaymentForm.js
+++ frontend/src/PaymentForm.js
@@ -7,6 +7,26 @@ import {
 } from "@stripe/react-stripe-js";
 import axios from "axios";
 
+const CARD_OPTIONS = {
+  iconStyle: "solid",
+  style: {
+    base: {
+      iconColor: "#c4f0ff",
+      color: "#fff",
+      fontWeight: 500,
+      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+      fontSize: "16px",
+      fontSmoothing: "antialiased",
+      ":-webkit-autofill": { color: "#fce883" },
+      "::placeholder": { color: "#87bbfd" },
+    },
+    invalid: {
+      iconColor: "#ffc7ee",
+      color: "#ffc7ee",
+    },
+  },
+};
+
 export default function PaymentForm() {
   const [success, setSuccess] = useState(false);
   const stripe = useStripe();
@@ -40,11 +60,22 @@ export default function PaymentForm() {
     }
   };
 
-  return ( 
-  <> 
-  {!success ? 
-  <form> onSubmit={handleSubmit} </form> } 
+  return (
+    <>
+      {!success} ?{" "}
+      <form onSubmit={handleSubmit}>
+        <fieldset className="FormGroup">
+          <div className="FormRow">
+            <CardElement options={CARD_OPTIONS} />
+          </div>
+        </fieldset>
 
-</>
-  )
+        <button>Pay</button>
+      </form>
+      :
+      <div>
+        <h2>Payment successful! Have a great stay at Andy's house!</h2>
+      </div>
+    </>
+  );
 }
